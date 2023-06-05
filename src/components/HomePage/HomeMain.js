cc05b11a38b2234cb034c5975787767a0f99b521
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2023-06-05 14:04:28 +0530

    file

---
+++ src/components/HomePage/HomeMain.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HomeMain = () => {
+    return (
+        <div className='main'>
+            <div className='main-container'>
+                <div className='main-content'>
+                    <h3>Medium is a place <br/>
+                    to write, read, and <br/>
+                    connec</h3>
+                <h6>It's easy and free to post your thinking on any topic and connectwith millions of reader.</h6>
+                <button>Start Writing</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default HomeMain
